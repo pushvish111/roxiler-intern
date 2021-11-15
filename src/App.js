b203ec633b2a6aadcfb4a6dfd28e183c2626cd81
Faisal <faisalalik9@gmail.com>
2021-11-15 17:32:14 +0530

    Search Bar Done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import UserDetail from './components/UserDetail';
 function App() {
   const[todos,setTodos]=useState();
   const[currUser,setCurrUser]=useState(1);
+  const[searchData,setSearchData]=useState();
  
 
   useEffect(()=>{
@@ -34,12 +35,30 @@ function App() {
   },[]);
 
 
+  function compare(temp){
+    var str = String(temp);
+    
+    console.log(str + ":::: and the value ius : "+ str.includes(searchData))
+    if(searchData !== undefined && str.includes(searchData) ){
+      return true;
+    }
+
+    return false;
+
+  }
+
+
 
   return (
     <div className="app">
 
       <NavBar />
 
+      <div className="search-bar-div">
+        <div className="search-bar">
+          <input onChange={(event)=>{setSearchData(event.target.value); console.log(typeof searchData)}} type="search" placeholder="Seach...."></input>
+        </div>
+      </div>
 
       <div className="app-body">
         <div className="left">
@@ -54,6 +73,7 @@ function App() {
               <tbody>
                 {todos?.map(row=>{
                 
+                    if(searchData === undefined || searchData === '')
                     return(
 
                       <tr key={row.id} className="left-tr">
@@ -64,6 +84,20 @@ function App() {
                       </tr>
 
                     )
+                    else{
+                      if(String(row.userId) === searchData  || (row.completed ? searchData === "Complete" : searchData === "Incomplete") || compare(row.title)){
+                          return(
+
+                              <tr key={row.id} className="left-tr">
+                                <td className="left-td-list l1">{row.userId}</td>
+                                <td className="left-td-list l2">{row.title}</td>
+                                <td className="left-td-list l3">{row.completed ? "Complete" : "Incomplete"}</td>
+                                <td className="left-td-list l4"><button onClick={()=>{setCurrUser(row)}}>View User</button></td>
+                              </tr>
+
+                          )
+                      }
+                    }
 
 
                 })}
